ab6864b35e1f38bbeefa1c9ff317cfd8eb316108
zweiper <jessicaerasmo21@gmail.com>
2023-11-20 22:18:36 +0800

    Day 1 of Backend CodeQuest Challenge
    Learned how to assign value to variables and how to properly print it using print and print f

---
+++ submissions/challenges/day-1-challenge.py
@@ -0,0 +1,6 @@
+name = "Jessica Danielle Erasmo"
+age = 20
+isMale = False
+department = "Software and Web Development"
+
+print(f"{name} is a {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
