3c9fbbe68aee8320da2b04a36a927aff4a17c439
zweiper <jessicaerasmo21@gmail.com>
2023-11-21 18:55:35 +0800

    Day 2 of CodeQuest: Using input and data type integer
    Simple program which adds three integer values from the user and displays it using the print and input functions.

---
+++ submissions/challenges/day-2-challenge.py
@@ -0,0 +1,7 @@
+print("Please enter the following: ")
+num1 = input("First Number: ")
+num2 = input("Second number: ")
+num3 = input("Third number: ")
+sum = num1 + num2 + num3
+print(f"\nSolution:\n{num1} + {num2} + {num3} = {sum}")
+print(f"\nAnswer:\nThe sum of three numbers is: {sum}\n")
