1a55b08b0a1e4316f7014eeac2be89f6ddf27721
zweiper <jessicaerasmo21@gmail.com>
2023-11-21 19:00:33 +0800

    Adding int data type
    Apparently, I forgot to use the int data type. Without it, the program will just concatenate the num variables as string and will not add it as integers.

--- submissions/challenges/day-2-challenge.py
+++ submissions/challenges/day-2-challenge.py
@@ -1,7 +1,7 @@
 print("Please enter the following: ")
-num1 = input("First Number: ")
-num2 = input("Second number: ")
-num3 = input("Third number: ")
+num1 = int(input("First Number: "))
+num2 = int(input("Second number: "))
+num3 = int(input("Third number: "))
 sum = num1 + num2 + num3
 print(f"\nSolution:\n{num1} + {num2} + {num3} = {sum}")
 print(f"\nAnswer:\nThe sum of three numbers is: {sum}\n")
