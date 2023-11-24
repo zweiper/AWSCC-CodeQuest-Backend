f726ff1857caeaecf590564e458a8ef392d14988
zweiper <jessicaerasmo21@gmail.com>
2023-11-24 22:39:54 +0800

    Added against computer codes
    Added the option to play against computer or player and added the needed functionalities if it is against computer

--- submissions/challenges/day-5-challenge.py
+++ submissions/challenges/day-5-challenge.py
@@ -1,9 +1,13 @@
 import os
+import random
 
 p1 = "Player 1"
 p2 = "Player 2"
+v_ai = "Computer"
+choices = ["rock", "paper", "scissors"]
 choice = None
 choice2 = None
+ans = None
 
 print("================================================================================================")
 print("|                                                                                              |")
@@ -14,42 +18,82 @@ print("=========================================================================
 print("\nWelcome to Rock, Paper, n' Scissors!")
 print("\nThe rules are simple:\n\tRock is strong against scissors but weak against paper!\n\tPaper is strong against rock but weak against scissors!\n\tScissors are strong against paper but weak against rock!")
 
-print(f"\nYour turn {p1}: Rock, Paper, or Scissors?")
-choice = input(f"{p1}: ")
-os.system('cls')
-print(f"Your turn {p2}: Rock, Paper, or Scissors?")
-choice2 = input(f"{p2}: ")
-os.system('cls')
+print("Would you like to play against computer? (yes [1] | no [0])")
+ans = input("Answer: ")
 
-print(f"{p1}: {choice}")
-print(f"{p2}: {choice2}\n")
+if ans == 0:
+    print(f"\nYour turn {p1}: Rock, Paper, or Scissors?")
+    choice = input(f"{p1}: ").lower()
+    os.system('cls')
+    print(f"Your turn {p2}: Rock, Paper, or Scissors?")
+    choice2 = input(f"{p2}: ").lower()
+    os.system('cls')
 
-if choice == "Rock" or choice == "rock" or choice == "rocks" or choice == "Rocks":
-    if choice2 == "Rock" or choice2 == "rock" or choice2 == "rocks" or choice2 == "Rocks":
-        print("Draw")
-    elif choice2 == "Paper" or choice2 == "Papers" or choice2 == "paper" or choice2 == "papers":
-        print(f"{p2} wins!")
-    elif choice2 == "Scissors" or choice2 == "Scissor" or choice2 == "scissors" or choice2 == "scissor":
-        print(f"{p1} wins!")
+    print(f"{p1}: {choice}")
+    print(f"{p2}: {choice2}\n")
+
+    if choice == "Rock" or choice == "rocks":
+        if choice2 == "rock" or choice2 == "rocks":
+            print("Draw")
+        elif choice2 == "Paper" or choice2 == "Papers":
+            print(f"{p2} wins!")
+        elif choice2 == "Scissors" or choice2 == "Scissor":
+            print(f"{p1} wins!")
+        else:
+            print("That's not one of the choices! Now the game will end because of you :c")
+    elif choice == "Paper" or choice == "Papers":
+        if choice2 == "Paper" or choice2 == "Papers":
+            print("Draw")
+        elif choice2 == "rock" or choice2 == "rocks":
+            print(f"{p1} wins!")
+        elif choice2 == "Scissors" or choice2 == "Scissor":
+            print(f"{p2} wins!")
+        else:
+            print("That's not one of the choices! Now the game will end because of you :c")
+    elif choice == "Scissors" or choice == "Scissor":
+        if choice2 == "Scissors" or choice2 == "Scissor":
+            print("Draw")
+        elif choice2 == "rock" or choice2 == "rocks":
+            print(f"{p2} wins!")
+        elif choice2 == "Paper" or choice2 == "Papers":
+            print(f"{p1} wins!")
+        else:
+            print("That's not one of the choices! Now the game will end because of you :c")
     else:
         print("That's not one of the choices! Now the game will end because of you :c")
-elif choice == "Paper" or choice == "Papers" or choice == "paper" or choice == "papers":
-    if choice2 == "Paper" or choice2 == "Papers" or choice2 == "paper" or choice2 == "papers":
-        print("Draw")
-    elif choice2 == "Rock" or choice2 == "rock" or choice2 == "rocks" or choice2 == "Rocks":
-        print(f"{p1} wins!")
-    elif choice2 == "Scissors" or choice2 == "Scissor" or choice2 == "scissors" or choice2 == "scissor":
-        print(f"{p2} wins!")
+elif ans == 1:
+    print(f"\nYour turn {p1}: Rock, Paper, or Scissors?")
+    choice = input(f"{p1}: ").lower()
+    print(f"\nYour turn {v_ai}: Rock, Paper, or Scissors?")
+    choice2 = random.choice(choices).lower()
+
+    if choice == "Rock" or choice == "rocks":
+        if choice2 == "rock" or choice2 == "rocks":
+            print("Draw")
+        elif choice2 == "Paper" or choice2 == "Papers":
+            print(f"{v_ai} wins!")
+        elif choice2 == "Scissors" or choice2 == "Scissor":
+            print(f"{p1} wins!")
+        else:
+            print("That's not one of the choices! Now the game will end because of you :c")
+    elif choice == "Paper" or choice == "Papers":
+        if choice2 == "Paper" or choice2 == "Papers":
+            print("Draw")
+        elif choice2 == "rock" or choice2 == "rocks":
+            print(f"{p1} wins!")
+        elif choice2 == "Scissors" or choice2 == "Scissor":
+            print(f"{v_ai} wins!")
+        else:
+            print("That's not one of the choices! Now the game will end because of you :c")
+    elif choice == "Scissors" or choice == "Scissor":
+        if choice2 == "Scissors" or choice2 == "Scissor":
+            print("Draw")
+        elif choice2 == "rock" or choice2 == "rocks":
+            print(f"{v_ai} wins!")
+        elif choice2 == "Paper" or choice2 == "Papers":
+            print(f"{p1} wins!")
+        else:
+            print("That's not one of the choices! Now the game will end because of you :c")
     else:
         print("That's not one of the choices! Now the game will end because of you :c")
-elif choice == "Scissors" or choice == "Scissor" or choice == "scissors" or choice == "scissor":
-    if choice2 == "Scissors" or choice2 == "Scissor" or choice2 == "scissors" or choice2 == "scissor":
-        print("Draw")
-    elif choice2 == "Rock" or choice2 == "rock" or choice2 == "rocks" or choice2 == "Rocks":
-        print(f"{p2} wins!")
-    elif choice2 == "Paper" or choice2 == "Papers" or choice2 == "paper" or choice2 == "papers":
-        print(f"{p1} wins!")
-    else:
-        print("That's not one of the choices! Now the game will end because of you :c")
-else:
-    print("That's not one of the choices! Now the game will end because of you :c")
+
