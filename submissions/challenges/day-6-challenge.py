98824defeda1266f12afc3ff8d4a3d136cf24c2d
zweiper <jessicaerasmo21@gmail.com>
2023-11-25 23:49:24 +0800

    Intro for Fizzbuzz
    Added simple intro for fizzbuzz

---
+++ submissions/challenges/day-6-challenge.py
@@ -0,0 +1,11 @@
+high_limit = None
+
+print("================================================================================================")
+print("|                                                                                              |")
+print("|                                         FizzBuzz!                                            |")
+print("|                                                                                              |")
+print("================================================================================================")
+
+print("\nWelcome to FizzBuzz!")
+
+print("\nRules of FizzBuzz:\n\t1. If a number is divisible by 3, you get a 'Fizz'\n\t2. If a number is divisible by 5, you get a 'Buzz'\n\t3. If a number is both divisible by 3 and 5, you get a 'FizzBuzz!'")
