7723ac1321210ca76f6c55aa2a9a95809815bf90
zweiper <jessicaerasmo21@gmail.com>
2023-11-26 00:05:49 +0800

    Added the loop and condtions
    Used for loop with if, elif, and else conditions to test whether the input is divisible by 3, 5, or both

--- submissions/challenges/day-6-challenge.py
+++ submissions/challenges/day-6-challenge.py
@@ -9,3 +9,20 @@ print("=========================================================================
 print("\nWelcome to FizzBuzz!")
 
 print("\nRules of FizzBuzz:\n\t1. If a number is divisible by 3, you get a 'Fizz'\n\t2. If a number is divisible by 5, you get a 'Buzz'\n\t3. If a number is both divisible by 3 and 5, you get a 'FizzBuzz!'")
+
+high_limit=int(input("\nEnter a number limit: "))
+
+for i in range(high_limit):
+    i+=1
+    if i%3 == 0:
+        if i%5 == 0:
+            print("FizzBuzz!")
+        else:
+            print("Fizz")
+    elif i%5 == 0:
+        if i%3 == 0:
+            print("FizzBuzz!")
+        else:
+            print("Buzz")
+    else:
+        print(f"{i}")
