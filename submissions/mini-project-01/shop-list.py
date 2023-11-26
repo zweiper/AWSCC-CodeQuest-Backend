c2db4068f54c277d12628d3ef89908e4a2cf499e
zweiper <jessicaerasmo21@gmail.com>
2023-11-26 18:08:09 +0800

    shopping-list mini-project
    created the file, and two functions for the project

---
+++ submissions/mini-project-01/shop-list.py
@@ -0,0 +1,21 @@
+choice = 0
+itemList = []
+
+def options():
+    print("Options:")
+    print("\t1. Add item to the shopping list")
+    print("\t2. View shopping list")
+    print("\t3. Remove item from the shopping list")
+    print("\t4. Quit")
+
+def switch(choice):
+    if choice == 1:
+        print("")
+    elif choice == 2:
+        print("")
+    elif choice == 3:
+        print("")
+    elif choice == 4:
+        print("")
+    else:
+        print("")
