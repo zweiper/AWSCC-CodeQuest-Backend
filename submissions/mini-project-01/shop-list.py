3da40200006b2c77b0300b6c5c6fec22f2c48fed
zweiper <jessicaerasmo21@gmail.com>
2023-11-26 18:34:35 +0800

    View List Function
    Added function for viewing the shopping list of the user

--- submissions/mini-project-01/shop-list.py
+++ submissions/mini-project-01/shop-list.py
@@ -9,12 +9,15 @@ def options():
     print("\t3. Remove item from the shopping list")
     print("\t4. Quit")
 
+    choice = int(input("\nEnter the number of your choice: "))
+    switchCase(choice)
+
 def switchCase(choice):
     if choice == 1:
-        itemAdd = input("Enter the item you want to add: ")
-        AddItem(itemAdd)       
+        itemAdd = input("Enter the item you want to add: ").lower()
+        AddItem(itemAdd)      
     elif choice == 2:
-        print("")
+        ViewList()
     elif choice == 3:
         print("")
     elif choice == 4:
@@ -26,6 +29,11 @@ def AddItem(itemAdd):
     itemList.append(itemAdd)
     print(f"{itemAdd} has been added to your shopping list")
 
+def ViewList():
+    if len(itemList) == 0:
+        print("Your shopping list is empty")
+    else:
+        print(f"Your shopping list:\n{itemList}")
+
 options()
-choice = input("Enter the number of your choice: ")
-switchCase(choice)
+
