ffd30ed88602f0e20dcaa5330e9de97c955748a8
zweiper <jessicaerasmo21@gmail.com>
2023-12-02 11:52:52 +0800

    Placed the codes inside a function
    Defining the codes in their separate function helps for a cleaner look

--- submissions/challenges/day-11-challenge/day-11-challenge.py
+++ submissions/challenges/day-11-challenge/day-11-challenge.py
@@ -1,12 +1,17 @@
 sortList = []
 namesList = []
 
-with open('D:\Github\AWSCC-CodeQuest-Backend\submissions\challenges\day-11-challenge\studentList.txt', 'r') as file:
-    names = file.readlines()
-    for line in names:
-        namesList.append(line)
-        sortList = sorted(namesList)
+def rewrite(sortList):
+    with open('D:\Github\AWSCC-CodeQuest-Backend\submissions\challenges\day-11-challenge\studentList.txt', 'w') as file:
+        file.writelines(sortList)
+        file.close()
 
-with open('D:\Github\AWSCC-CodeQuest-Backend\submissions\challenges\day-11-challenge\studentList.txt', 'w') as file:
-    file.writelines(sortList)
-    file.close()
+def sort():
+    with open('D:\Github\AWSCC-CodeQuest-Backend\submissions\challenges\day-11-challenge\studentList.txt', 'r') as file:
+        names = file.readlines()
+        for line in names:
+            namesList.append(line)
+            sortList = sorted(namesList)
+            rewrite(sortList)
+        
+sort()
